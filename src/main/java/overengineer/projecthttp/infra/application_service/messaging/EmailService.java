6108a30317a6bb3b4282a6cb0d57f361de1418bf
Lucas <lucas.jdev1@gmail.com>
2024-11-07 08:17:10 -0300

    refact: new methods for send and receive email

--- src/main/java/overengineer/projecthttp/infra/application_service/messaging/EmailService.java
+++ src/main/java/overengineer/projecthttp/infra/application_service/messaging/EmailService.java
@@ -1,21 +1,23 @@
 package overengineer.projecthttp.infra.application_service.messaging;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import overengineer.projecthttp.infra.rabbitmq.Email;
+import overengineer.projecthttp.infra.use_case.crud.email.SaveEmailAsReceived;
+import overengineer.projecthttp.infra.use_case.crud.email.SaveEmailAsSent;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
-public class EmailService {
+public record EmailService (
+    JavaMailSender javaMailSender,
+    SaveEmailAsSent saveEmailAsSent,
+    SaveEmailAsReceived saveEmailAsReceived
+) {
 
-    private final JavaMailSender javaMailSender;
-
-    public void sendEmail(Email email) {
+    public void sendEmail(@NonNull Email email) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(email.getEmailFrom());
         simpleMailMessage.setTo(email.getEmailTo());
@@ -23,7 +25,13 @@ public class EmailService {
         simpleMailMessage.setText(email.getBody());
 
         javaMailSender.send(simpleMailMessage);
+        saveEmailAsSent.execute(email);
         log.info("Sending email to: {}", email.getEmailTo());
+    }
+
+    public void receiveEmail(@NonNull Email email) {
+        saveEmailAsReceived.execute(email.getId().toString());
+        log.info("Receiving email from: {}", email.getEmailFrom());
     }
 
 }
