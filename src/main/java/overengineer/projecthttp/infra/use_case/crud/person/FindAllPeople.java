496f15b67c2daae080df9dffde7fc17bf5fc5ed4
Lucas <lucas.jdev1@gmail.com>
2024-11-21 14:49:17 -0300

    refact: adjustments in use case of find all people

--- src/main/java/overengineer/projecthttp/infra/use_case/crud/person/FindAllPeople.java
+++ src/main/java/overengineer/projecthttp/infra/use_case/crud/person/FindAllPeople.java
@@ -1,10 +1,14 @@
 package overengineer.projecthttp.infra.use_case.crud.person;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import overengineer.projecthttp.domain.person.PersonFilter;
 import overengineer.projecthttp.domain.person.PersonGateway;
+import overengineer.projecthttp.infra.mapper.PersonMapper;
 import overengineer.projecthttp.infra.use_case.crud.person.dto.PersonFound;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Service
 public record FindAllPeople(PersonGateway repo){
@@ -12,10 +16,15 @@ public record FindAllPeople(PersonGateway repo){
     public Collection<PersonFound> execute() {
         return repo.findAll()
             .stream()
-            .map(person -> new PersonFound(person.getId(), person.getName(),
-                    person.getLastName(), person.getBirthDate(),
-                    person.getEmail(), person.isActive()))
+            .map(PersonMapper::domainToOutputFound)
             .toList();
+    }
+
+    public Collection<PersonFound> execute(@NonNull PersonFilter<?> filter, @NonNull Map<String, Object> params){
+        return repo.findAll(filter, params)
+                .stream()
+                .map(PersonMapper::domainToOutputFound)
+                .toList();
     }
 
 }
